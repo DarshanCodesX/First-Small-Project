953ab70d2286108816a5627ea81b53023f228867
Darshan Aloor <darshanaloor545@gmail.com>
2026-02-20 21:19:10 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Traffic {
-    public void signal(int time) {
-        while (true) {
+    public void signal(int time,int cycles) {
+      for(int i=1;i<=cycles;i++) {
             try {
                 System.out.println("RED Light — STOP");
                   countdown(time);
@@ -27,11 +27,19 @@ public class Project{
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter timer value: ");
-        int num = sc.nextInt();
+        int timer = sc.nextInt();
+        System.out.print("Enter the number of Cycles:");
+        int cycles=sc.nextInt();
+        if(timer>0&& cycles>0){
         Traffic tra = new Traffic();
-        tra.signal(num);
+        tra.signal(timer,cycles);
+        }
+        else{
+            System.out.print("Invalid input");
+        }
         sc.close();
     }
 
 }
+
 
